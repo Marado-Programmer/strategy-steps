d2e747d53452ab15ad1f13bfabcae1f40a6c1553
Marado-Programer <marado.pen.eng@gmail.com>
2022-06-19 23:30:15 +0100

    forgot to add the command file

---
+++ client/commands/connect.js
@@ -0,0 +1,43 @@
+module.exports = {
+  name: "connect",
+  aliases: [ "conn", ],
+  requirements: [],
+  description: {
+    base: "Connect to a socket with the given arguments or by using the configuration file.",
+    extended: "Use the -p flag to specify the port and the -h flag to specify the host.\nThe priority of the configuration goes to the flags.",
+  },
+  execution: (service, ...args) => {
+    let configuration;
+    if (!args.includes("--ignore-config")) {
+      const configurationFile = args.includes("-c") ? args[args.indexOf("-c") + 1] : "../../config.js";
+
+      try {
+        configuration = require(configurationFile).connectOptions;
+      } catch (e) {
+        return {
+          message: "Couldn't find the configuration file.",
+          error: e,
+        };
+      }
+    }
+
+    if (args.includes("-p"))
+      configuration.port = args[args.indexOf("-p") + 1];
+
+    if (args.includes("-h"))
+      configuration.host = args[args.indexOf("-h") + 1];
+
+    try {
+      service.init(configuration);
+    } catch (e) {
+      return {
+        message: "Couldn't connect to the Socket.",
+        error: e,
+      };
+    }
+
+    return {
+      message: "Connected to the Socket",
+    };
+  }
+}
